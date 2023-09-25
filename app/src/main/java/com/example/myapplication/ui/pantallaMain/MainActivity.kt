73b2c9e40836b3429340dd0d3b0c6f3e39eb90c6
Rey M. Ortiz <rmopix@gmail.com>
2023-09-25 19:02:41 +0200

    casa

--- app/src/main/java/com/example/myapplication/ui/pantallaMain/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/pantallaMain/MainActivity.kt
@@ -1,16 +1,24 @@
-package com.example.myapplication
+package com.example.myapplication.ui.pantallaMain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding;
+
     private lateinit var txt:EditText;
     private lateinit var boton: Button;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+        }
         setContentView(R.layout.activity_main)
 
         txt = findViewById(R.id.editTextText)
